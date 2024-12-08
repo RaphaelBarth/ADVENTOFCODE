8027b4240fb043ac71734858a00134db6053b940
Jennifer Miller <raphael.t.barth+github@gmail.com>
2024-12-08 10:41:23 +0100

    update Day6

--- 2024/Day6/Day6_Guard-Gallivant.py
+++ 2024/Day6/Day6_Guard-Gallivant.py
@@ -1,5 +1,6 @@
 from enum import Enum, StrEnum
 import re
+from time import *
 import numpy as np
 
 temp = """....#.....
@@ -12,6 +13,7 @@ temp = """....#.....
 ........#.
 #.........
 ......#..."""
+DEBUG_PRINTING = False
 
 class DIRECTION(StrEnum):
     UP='0'
@@ -23,83 +25,60 @@ class DIRECTION(StrEnum):
         x = (int(self.value)+1)%4
         return DIRECTION(str(x))
 
+def walk_field(field,limit = None):
+    pos_y,pos_x = np.where(field=="^")
+    direction = DIRECTION.UP
+
+    limit_ctn =  -1 if not limit else limit
+    while limit_ctn:
+        limit_ctn = limit_ctn - 1  
+
+        field[pos_y,pos_x] = 'x'
+        match direction:
+            case DIRECTION.UP:
+                new_pos_y,new_pos_x = pos_y-1,pos_x
+            case DIRECTION.DOWN:
+                new_pos_y,new_pos_x = pos_y+1,pos_x
+            case DIRECTION.LEFT:
+                new_pos_y,new_pos_x = pos_y,pos_x-1
+            case DIRECTION.RIGHT:
+                new_pos_y,new_pos_x = pos_y,pos_x+1
+
+        if new_pos_x == -1 or new_pos_y == -1 or new_pos_x  == field.shape[1] or new_pos_y == field.shape[0]:
+            return field
+    
+        if field[new_pos_y,new_pos_x] == '#':
+            if DEBUG_PRINTING:
+                np.savetxt(fname="temp.txt",X=field,fmt="%s")
+            direction = direction.new_direction()
+        else:
+            pos_x,pos_y = new_pos_x,new_pos_y
+    # we reached the limit
+    return None
+
+
+
+
 with open("2024/Day6/Day6_data.txt") as file:
     data = file.read()
     field = np.array([list(line.strip()) for line in data.split('\n')])
 
-    pos_y,pos_x = np.where(field=="^")
-    direction = DIRECTION.UP
+    # part1
+    resuld_field = walk_field(field.copy())
+    print(np.count_nonzero(resuld_field == 'x'))
 
-    while True:
-        field[pos_y,pos_x] = 'x'
-        match direction:
-            case DIRECTION.UP:
-                new_pos_y,new_pos_x = pos_y-1,pos_x
-            case DIRECTION.DOWN:
-                new_pos_y,new_pos_x = pos_y+1,pos_x
-            case DIRECTION.LEFT:
-                new_pos_y,new_pos_x = pos_y,pos_x-1
-            case DIRECTION.RIGHT:
-                new_pos_y,new_pos_x = pos_y,pos_x+1
-
-        if new_pos_x == -1 or new_pos_y == -1 or new_pos_x  == field.shape[1] or new_pos_y == field.shape[0]:
-            break
-    
-        if field[new_pos_y,new_pos_x] != '#':
-            pos_x,pos_y = new_pos_x,new_pos_y
+    # part2
+    obstacles = 0
+    guard_y,guard_x = np.where(field=="^")
+    for iy, ix in np.ndindex(field.shape):
+        if guard_x == ix and guard_y == iy:
+            pass
         else:
-            direction = direction.new_direction()
+            temp_field = field.copy()
+            temp_field[iy,ix] = '#'
+            obstacles += 1 if walk_field(temp_field,limit=10_000) is None else 0
+            #print((iy, ix),obstacles)
 
-    print(np.count_nonzero(field == 'x'))
-
-
-with open("2024/Day6/Day6_data.txt") as file:
-    data = temp#file.read()
-    field = np.array([list(line.strip()) for line in data.split('\n')])
-
-    pos_y,pos_x = np.where(field=="^")
-    pos_y,pos_x = pos_y[0] ,pos_x[0]
-    direction = DIRECTION.UP
-    positions =0
-    
-    while True:
-        field[pos_y,pos_x] = 'x'
-        match direction:
-            case DIRECTION.UP:
-                new_pos_y,new_pos_x = pos_y-1,pos_x
-            case DIRECTION.DOWN:
-                new_pos_y,new_pos_x = pos_y+1,pos_x
-            case DIRECTION.LEFT:
-                new_pos_y,new_pos_x = pos_y,pos_x-1
-            case DIRECTION.RIGHT:
-                new_pos_y,new_pos_x = pos_y,pos_x+1
-                
-        if new_pos_x == -1 or new_pos_y == -1 or new_pos_x  == field.shape[1] or new_pos_y == field.shape[0]:
-            break
-        
-        #np.savetxt(fname="temp.txt",X=field,fmt="%s")
-        match direction:
-            case DIRECTION.UP:
-                lis = [ele for ele in field[pos_y,pos_x+1:]]
-            case DIRECTION.DOWN:
-                lis = [ele for ele in field[pos_y,pos_x-1::-1]]
-            case DIRECTION.LEFT:
-                lis = [ele for ele in field[pos_y-1::-1,pos_x] ]
-            case DIRECTION.RIGHT:
-                lis = [ele for ele in field[pos_y+1::,pos_x]]
-
-        x = str.join("",lis)
-        if lis and re.findall(".*x#",x):
-            positions+=1
-        
-
-        if field[new_pos_y,new_pos_x] != '#':
-            pos_x,pos_y = new_pos_x,new_pos_y
-        else:
-            direction = direction.new_direction()
-            #field[field==direction]='.'
-
-
-    print(positions)
+    print(f"{obstacles=}")
 
 
