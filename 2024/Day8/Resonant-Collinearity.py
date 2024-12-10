90cd94e961b328d4ac8bf4c78512cc03cdfa0e30
RaphaelBarth <raphael.t.barth+github@gmail.com>
2024-12-10 13:22:30 +0100

    day 8

---
+++ 2024/Day8/Resonant-Collinearity.py
@@ -0,0 +1,70 @@
+from itertools import combinations
+import numpy as np
+
+temp = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"""
+
+def is_out_of_bound(index,grid):
+    y,x = grid.shape
+    if index[0] >= y or index[0] < 0:
+        return True
+    if index[1] >= x or index[1] < 0:
+        return True
+    return False
+
+with open("2024\Day8\data.txt") as file:
+    data = file.read()
+    # get the frequencies
+    frequencies  = set(data.replace(".","").replace("\n",""))
+    print(f"{frequencies=}")
+    # convert the input to np array
+    antenna_grid = np.array([list(line.strip()) for line in data.split('\n')])
+
+    # extract some caluculation data
+    antennas =  {frequency:np.transpose((antenna_grid == frequency).nonzero()) for frequency in frequencies}
+    for frequencie,antenna in antennas.items():
+            antennas[frequencie] = list(combinations(antenna,2))
+
+    # part 1
+    antinode_grid = np.zeros(antenna_grid.shape,dtype=int)
+    for frequencie,antenna_combinations in antennas.items():
+        for antenna_combination in antenna_combinations:
+            offset = np.subtract(antenna_combination[0], antenna_combination[1])
+            antinode1 = np.subtract(antenna_combination[1],offset)
+            antinode2 = np.add(antenna_combination[0],offset)
+            if not is_out_of_bound(antinode1,antenna_grid):
+                antinode_grid[tuple(antinode1)] = 1
+            if not is_out_of_bound(antinode2,antenna_grid):
+                antinode_grid[tuple(antinode2)] = 1
+    # print the result 
+    print(np.sum(antinode_grid,where=[1]))
+
+
+    # part 2
+    antinode_grid = np.zeros(antenna_grid.shape,dtype=int)
+    for frequencie,antenna_combinations in antennas.items():
+        for antenna_combination in antenna_combinations:
+            offset = np.subtract(antenna_combination[0], antenna_combination[1])
+            for step in range(antinode_grid.shape[0]):
+                    new_offset = offset*step
+                    antinode1 = np.subtract(antenna_combination[1],new_offset)
+                    antinode2 = np.add(antenna_combination[0],new_offset)
+                    if not is_out_of_bound(antinode1,antenna_grid):
+                        antinode_grid[tuple(antinode1)] = 1
+                    if not is_out_of_bound(antinode2,antenna_grid):
+                        antinode_grid[tuple(antinode2)] = 1
+    print(np.sum(antinode_grid,where=[1]))
+
+
+
+
